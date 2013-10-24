31904c215de0f0d1474138695adc9fd65e6e5449
euromark <euromark@web.de>
2013-10-25 01:26:27 +0200

    add more sniffs and tests

---
+++ Vendor/PHP/CodeSniffer/Standards/CakePHP/Test/files/concatenation_spacing_expected.php
@@ -0,0 +1,19 @@
+<?php
+
+class Foo {
+
+	public function test() {
+		$x = $y . $x;
+
+		$y = 'some string'  .  'more';
+	}
+
+	public function test2() {
+		$x = $y
+			. $x;
+
+		$y = 'some string' .
+			'more';
+	}
+
+}
