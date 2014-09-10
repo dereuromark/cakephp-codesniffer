9548dd2e8ac621f88c7bb603d48946b40ae1166c
euromark <euromark@web.de>
2014-09-10 16:02:07 +0200

    fix test file

--- Vendor/PHP/CodeSniffer/Standards/MyCakePHP/Test/files/cast_fail.php
+++ Vendor/PHP/CodeSniffer/Standards/MyCakePHP/Test/files/cast_fail.php
@@ -6,10 +6,8 @@
 class Foo {
 
 	public function aMethod() {
-		$a = (int)$y;
-		$a = (float)$y;
-		$a = (int)$foo;
-		$a = (float)$foo;
+		$a = intval($y);
+		$a = floatval($y);
 
 		$half = intval($modulus / 2);
 
@@ -18,9 +16,9 @@ class Foo {
 	}
 
 	public function anotherMethod() {
-		$a = $x + ((int)$y);
+		$a = $x + (intval($y));
 		$a = $x + (intval($y, 9));
-		$a = $x + ((int)SomeClass::foo($b, $c, $d));
+		$a = $x + (intval(SomeClass::foo($b, $c, $d)));
 		$a = $x + (intval(SomeClass::foo($b, $c, $d), 9));
 	}
 
