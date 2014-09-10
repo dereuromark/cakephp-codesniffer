a1f2e959955bb951655448f79bdf5e9c8a775cb7
euromark <euromark@web.de>
2014-09-10 16:30:53 +0200

    refine sniffer

--- Vendor/PHP/CodeSniffer/Standards/MyCakePHP/Sniffs/PHP/CastSniff.php
+++ Vendor/PHP/CodeSniffer/Standards/MyCakePHP/Sniffs/PHP/CastSniff.php
@@ -55,17 +55,31 @@ class MyCakePHP_Sniffs_PHP_CastSniff implements PHP_CodeSniffer_Sniff {
 			return;
 		}
 
-		// For now we skip on non trivial cases
-		$continue = $lastToken <= ($nextToken + 2);
-		if (!$continue) {
-			return;
-		}
-
 		$error = 'Usage of ' . $tokens[$stackPtr]['content'] . ' not allowed; use casting instead';
 		$phpcsFile->addFixableError($error, $stackPtr, 'NotAllowed');
 
 		// Try to determine if we can remove the parentheses
 		$removeParentheses = $lastToken <= ($nextToken + 2);
+		if (!$removeParentheses) {
+			// It is also OK if we can extract scopes inside
+			$openerToken = $phpcsFile->findNext(T_OPEN_PARENTHESIS, ($nextToken + 1), $lastToken - 1);
+			if ($openerToken) {
+				$closerToken = $tokens[$openerToken]['parenthesis_closer'];
+				$count = $closerToken - $openerToken + 1;
+				if (($lastToken - $count) <= ($nextToken + 2)) {
+					$removeParentheses = true;
+				}
+			}
+		}
+		if (!$removeParentheses) {
+			// If there is no operator in between, it is also fine
+			$whitelist = array(T_PLUS, T_MINUS, T_MODULUS, T_MULTIPLY, T_DIVIDE, T_BITWISE_AND, T_BITWISE_OR, T_BOOLEAN_NOT, T_POWER);
+			$end = !empty($openerToken) ? $openerToken : ($lastToken - 1);
+			$operatorToken = $phpcsFile->findNext($whitelist, ($nextToken + 1), $end);
+			if (!$operatorToken) {
+				$removeParentheses = true;
+			}
+		}
 
 		// Fix the error
 		if ($phpcsFile->fixer->enabled === true) {
