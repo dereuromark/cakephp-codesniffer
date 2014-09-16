ea7165ad34c08db1dfb6a281b2d10f7352dd5423
euromark <euromark@web.de>
2014-09-16 23:50:16 +0200

    Add test

---
+++ tests/TestCase/Shell/CleanupShellTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace CodeSniffer\Test\TestCase\Shell;
+
+use Cake\TestSuite\TestCase;
+use Cake\Core\Configure;
+use Cake\Console\ConsoleOutput;
+use Cake\Console\ConsoleIo;
+use CodeSniffer\Shell\CleanupShell;
+
+/**
+ * Class TestCleanupShellOutput
+ */
+class TestCleanupShellOutput extends ConsoleOutput {
+
+	public $output = '';
+
+	protected function _write($message) {
+		$this->output .= $message;
+	}
+
+}
+
+class CleanupShellTest extends CakeTestCase {
+
+	public $Cleanup;
+
+	public $testPath;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->out = new TestCleanupShellOutput();
+		$io = new ConsoleIo($this->out);
+
+		$this->Cleanup = new CleanupShell($io);
+		$this->Cleanup->initialize();
+
+		$this->testFolder = dirname(dirname(dirname(__FILE__))) . DS . 'test_app' . DS;
+	}
+
+	/**
+	 * CodeSnifferShellTest::testTokenize()
+	 *
+	 * @return void
+	 */
+	public function testUnusedUse() {
+		$result = $this->Md->runCommand(array('unused_use'), true);
+		$this->assertSame(0, $result);
+
+		$result = $this->out->output;
+		debug($result);
+	}
+
+}
